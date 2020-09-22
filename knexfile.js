eba2c5aca9407c8e306572227ccb431a783702ca
Kyle Lovell <ky2vell@gmail.com>
2020-09-22 00:12:52 -0400

    pg setup

--- knexfile.js
+++ knexfile.js
@@ -1,4 +1,14 @@
 module.exports = {
+  production: {
+    client: 'pg',
+    connection: process.env.DATABASE_URL,
+    migrations: {
+      directory: './data/migrations'
+    },
+    seeds: {
+      directory: './data/seeds'
+    }
+  },
   development: {
     client: 'sqlite3',
     connection: { filename: './data/use_tech.db3' },
