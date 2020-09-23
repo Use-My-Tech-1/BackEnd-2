84a0aaa556f3ce0888ae1cb284f935fcf38f7e21
Kyle Lovell <ky2vell@gmail.com>
2020-09-23 01:22:25 -0400

    auth endpoints testing

---
+++ __tests__/auth.js
@@ -0,0 +1,81 @@
+const supertest = require('supertest');
+const server = require('../server');
+const db = require('../data/dbConfig');
+
+const OwnerRegCreds = {
+  username: 'newUser',
+  password: 'password',
+  email: 'mail@gmail.com'
+};
+
+const OwnerLoginCreds = {
+  username: 'newUser',
+  password: 'password'
+};
+
+beforeAll(async () => {
+  // run seeds before tests
+  await db.truncate('users');
+});
+
+afterAll(async () => {
+  // Close database after tests to prevent warning
+  await db.destroy();
+});
+
+describe('owners integration tests', () => {
+  it('POST /api/auth/register', async () => {
+    const res = await supertest(server)
+      .post('/api/auth/register')
+      .send(OwnerRegCreds);
+    expect(res.statusCode).toBe(201);
+    expect(res.type).toBe('application/json');
+    expect(res.body.username).toBe('newUser');
+  });
+
+  it('POST /api/auth/register (user already exists)', async () => {
+    const res = await supertest(server)
+      .post('/api/auth/register')
+      .send(OwnerRegCreds);
+    expect(res.statusCode).toBe(409);
+    expect(res.type).toBe('application/json');
+    expect(res.body.message).toBe('Username already taken');
+  });
+
+  it('POST /api/auth/register (missing fields)', async () => {
+    const res = await supertest(server)
+      .post('/api/auth/register')
+      .send({ username: 'newUser' });
+    expect(res.statusCode).toBe(400);
+    expect(res.type).toBe('application/json');
+    expect(res.body.message).toBe('Missing required fields');
+  });
+
+  it('POST /api/auth/login', async () => {
+    const res = await supertest(server)
+      .post('/api/auth/login')
+      .send(OwnerLoginCreds);
+    expect(res.statusCode).toBe(200);
+    expect(res.type).toBe('application/json');
+    expect(res.body.message).toBe('Welcome newUser!');
+  });
+
+  it('POST /api/auth/login (missing fields)', async () => {
+    const res = await supertest(server).post('/api/auth/login').send({
+      username: 'CoolDude'
+    });
+    expect(res.statusCode).toBe(400);
+    expect(res.type).toBe('application/json');
+    expect(res.body.message).toBe('Username & password fields are required');
+  });
+
+  it('POST /api/auth/login (user not found)', async () => {
+    const res = await supertest(server).post('/api/auth/login').send({
+      username: 'CoolDude',
+      password: 'password'
+    });
+    expect(res.statusCode).toBe(401);
+    expect(res.type).toBe('application/json');
+    expect(res.body.message).toBe('User not found');
+  });
+});
